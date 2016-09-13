22e3af8e1022f7b68f159575ab51cf51066d52cf
Neel Mehta <neelmehta@college.harvard.edu>
2016-09-13 14:56:58 -0400

    double prices

--- lab2/js/amusement.js
+++ lab2/js/amusement.js
@@ -53,3 +53,20 @@ console.log("Name of first ride:", amusementRides[0].name);
 console.log("All open days of 2nd ride:", amusementRides[1].openDays.join(", "));
 console.log("1st open day of 2nd ride:", amusementRides[1].openDays[0]);
 console.log("Reduced price of 3rd ride:", amusementRides[2].price / 2);
+
+// 2.1 doublePrices
+/**
+ * Double the prices of each ride, except the 2nd, in place, and returns the modified array.
+ */
+function doublePrices(amusementRides) {
+    for (var i = 0; i < amusementRides.length; i++) {
+        if (i != 1) {
+            amusementRides[i].price *= 2;
+        }
+    }
+
+    return amusementRides;
+}
+
+var amusementRidesDouble = doublePrices(amusementRides);
+console.log(amusementRidesDouble);
