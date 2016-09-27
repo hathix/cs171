a6dbd88726c7cc6fd636147f3001edd7086f17e5
Neel Mehta <neelmehta@college.harvard.edu>
2016-09-27 12:16:26 -0400

    beautify

--- lab4/js/main.js
+++ lab4/js/main.js
@@ -1,14 +1,12 @@
-
 // SVG Size
 var width = 700,
-		height = 500;
-
+  height = 500;
 
 // Load CSV file
-d3.csv("data/wealth-health-2014.csv", function(data){
+d3.csv("data/wealth-health-2014.csv", function(data) {
 
-	// Analyze the dataset in the web console
-	console.log(data);
-	console.log("Countries: " + data.length)
+  // Analyze the dataset in the web console
+  console.log(data);
+  console.log("Countries: " + data.length)
 
 });
