4ceb0780becd59e8f89c0207d81ced3519f245e5
Neel Mehta <neelmehta@college.harvard.edu>
2016-10-09 09:18:11 -0400

    add template

---
+++ lab5/activity2/js/main.js
@@ -0,0 +1,52 @@
+
+// SVG drawing area
+
+var margin = {top: 40, right: 10, bottom: 60, left: 60};
+
+var width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var svg = d3.select("#chart-area").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+// Scales
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+
+// Initialize data
+loadData();
+
+// Coffee chain data
+var data;
+
+// Load CSV file
+function loadData() {
+	d3.csv("data/coffee-house-chains.csv", function(error, csv) {
+
+		csv.forEach(function(d){
+			d.revenue = +d.revenue;
+			d.stores = +d.stores;
+		});
+
+		// Store csv data in global variable
+		data = csv;
+
+    // Draw the visualization for the first time
+		updateVisualization();
+	});
+}
+
+// Render visualization
+function updateVisualization() {
+
+  console.log(data);
+
+}
