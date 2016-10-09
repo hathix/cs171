72bcff108e1510aa901c62b816e8377eb859bb69
Neel Mehta <neelmehta@college.harvard.edu>
2016-10-09 10:06:07 -0400

    format

--- hw5/js/main.js
+++ hw5/js/main.js
@@ -1,16 +1,21 @@
-
 // SVG drawing area
 
-var margin = {top: 40, right: 40, bottom: 60, left: 60};
+var margin = {
+  top: 40,
+  right: 40,
+  bottom: 60,
+  left: 60
+};
 
 var width = 600 - margin.left - margin.right,
-		height = 500 - margin.top - margin.bottom;
+  height = 500 - margin.top - margin.bottom;
 
-var svg = d3.select("#chart-area").append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("#chart-area")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
 // Date parser (https://github.com/mbostock/d3/wiki/Time-Formatting)
@@ -26,38 +31,38 @@ var data;
 
 // Load CSV file
 function loadData() {
-	d3.csv("data/fifa-world-cup.csv", function(error, csv) {
+  d3.csv("data/fifa-world-cup.csv", function(error, csv) {
 
-		csv.forEach(function(d){
-			// Convert string to 'date object'
-			d.YEAR = formatDate.parse(d.YEAR);
-			
-			// Convert numeric values to 'numbers'
-			d.TEAMS = +d.TEAMS;
-			d.MATCHES = +d.MATCHES;
-			d.GOALS = +d.GOALS;
-			d.AVERAGE_GOALS = +d.AVERAGE_GOALS;
-			d.AVERAGE_ATTENDANCE = +d.AVERAGE_ATTENDANCE;
-		});
+    csv.forEach(function(d) {
+      // Convert string to 'date object'
+      d.YEAR = formatDate.parse(d.YEAR);
 
-		// Store csv data in global variable
-		data = csv;
+      // Convert numeric values to 'numbers'
+      d.TEAMS = +d.TEAMS;
+      d.MATCHES = +d.MATCHES;
+      d.GOALS = +d.GOALS;
+      d.AVERAGE_GOALS = +d.AVERAGE_GOALS;
+      d.AVERAGE_ATTENDANCE = +d.AVERAGE_ATTENDANCE;
+    });
 
-		// Draw the visualization for the first time
-		updateVisualization();
-	});
+    // Store csv data in global variable
+    data = csv;
+
+    // Draw the visualization for the first time
+    updateVisualization();
+  });
 }
 
 
 // Render visualization
 function updateVisualization() {
-	
-	console.log(data);
+
+  console.log(data);
 
 }
 
 
 // Show details for a specific FIFA World Cup
-function showEdition(d){
-	
+function showEdition(d) {
+
 }
