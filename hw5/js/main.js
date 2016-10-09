71a87e56db7ef8fa1ec41721cdd1cecfa2cb0a9e
Neel Mehta <neelmehta@college.harvard.edu>
2016-10-09 10:09:42 -0400

    add axes

--- hw5/js/main.js
+++ hw5/js/main.js
@@ -21,6 +21,33 @@ var svg = d3.select("#chart-area")
 // Date parser (https://github.com/mbostock/d3/wiki/Time-Formatting)
 var formatDate = d3.time.format("%Y");
 
+// Scales
+var x = d3.scale.ordinal()
+  .rangeRoundBands([width, 0], .1);
+
+var y = d3.scale.linear()
+  .range([height, 0]);
+
+// axes
+var xAxis = d3.svg.axis()
+  .scale(x)
+  .orient("bottom");
+var yAxis = d3.svg.axis()
+  .scale(y)
+  .orient("left");
+
+var xGroup = svg.append('g')
+  .attr('class', 'axis x-axis')
+  .attr("transform", "translate(" + 0 + "," + height +
+    ")");
+var yGroup = svg.append('g')
+  .attr('class', 'axis y-axis');
+// .attr("transform", "translate(" + (0) + "," + (0) + ")");
+
+// bars
+var barGroup = svg.append('g');
+// .attr("transform", "translate(" + margin + "," + margin.top + ")");
+
 
 // Initialize data
 loadData();
@@ -58,6 +85,22 @@ function loadData() {
 function updateVisualization() {
 
   console.log(data);
+
+  // update domains
+  // x: years
+  x.domain(data.map(function(d) {
+    return formatDate(d.YEAR);
+  }));
+  y.domain([0, d3.max(data.map(function(d) {
+    return d.AVERAGE_GOALS;
+  }))]);
+
+  // redraw axes
+  xGroup.transition()
+      .duration(1000).call(xAxis);
+  yGroup.transition()
+      .duration(1000).call(yAxis);
+
 
 }
 
