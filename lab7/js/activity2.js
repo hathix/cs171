2fb4fe5160a666ef2553794799cc238974168c3d
Neel Mehta <neelmehta@college.harvard.edu>
2016-10-18 15:31:59 -0400

    draw map

--- lab7/js/activity2.js
+++ lab7/js/activity2.js
@@ -1,14 +1,19 @@
 var width = 1000;
 var height = 600;
 
-var projection = d3.geo.mercator()
-  .translate([width / 2, height / 2])
-  .scale(100)
-  .clipExtent([
-// clip off of antarctica
-    [0, 0],
-    [width, height * 0.85]
-  ]);
+// Mercator projection
+// var projection = d3.geo.mercator()
+//   .translate([width / 2, height / 2])
+//   .scale(100)
+//   .clipExtent([
+//     // clip off of antarctica
+//     [0, 0],
+//     [width, height * 0.85]
+//   ]);
+
+// NEW equirectangular projection
+var projection = d3.geo.equirectangular()
+  .translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
   .projection(projection);
@@ -20,17 +25,38 @@ var svg = d3.select("#atlas-holder")
   .attr("height", height);
 
 // Load world atlas
-d3.json("data/world-110m.json", function(error, data) {
+queue()
+  .defer(d3.json, "data/world-110m.json")
+  .defer(d3.json, "data/airports.json")
+  .await(function(error, worldData, airportData) {
 
-  // Convert TopoJSON to GeoJSON (target object = 'countries')
-  var geoJSON = topojson.feature(data, data.objects.countries)
-    .features;
+    // draw map
+    // Convert TopoJSON to GeoJSON (target object = 'countries')
+    var geoJSON = topojson.feature(worldData, worldData.objects.countries)
+      .features;
 
-  // Render the world atlas by using the path generator
-  svg.selectAll("path")
-    .data(geoJSON)
-    .enter()
-    .append("path")
-    .attr("class", "map")
-    .attr("d", path);
-});
+    // Render the world atlas by using the path generator
+    svg.selectAll("path")
+      .data(geoJSON)
+      .enter()
+      .append("path")
+      .attr("class", "map")
+      .attr("d", path);
+
+
+    // draw airports
+    svg.selectAll(".airport")
+      .data(airportData.nodes)
+      .enter()
+      .append("circle")
+      .attr("class", "airport")
+      .attr("transform", function(d) {
+        return "translate(" + projection([d.longitude, d.latitude]) + ")"
+      })
+      .attr("r", 5)
+      .append("title")
+      .text(function(d) {
+        return d.name;
+      });
+
+  });
