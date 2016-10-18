5dc0286c224fdcc223b7bd9a2b7edfd9a298c9ac
Neel Mehta <neelmehta@college.harvard.edu>
2016-10-18 15:19:26 -0400

    draw map

--- lab7/js/activity2.js
+++ lab7/js/activity2.js
@@ -2,10 +2,22 @@ var width = 1000;
 var height = 600;
 
 var projection = d3.geo.mercator()
-  .translate([width / 2, height / 2]);
+  .translate([width / 2, height / 2])
+  .scale(100)
+  .clipExtent([
+// clip off of antarctica
+    [0, 0],
+    [width, height * 0.85]
+  ]);
 
 var path = d3.geo.path()
   .projection(projection);
+
+
+var svg = d3.select("#atlas-holder")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
 // Load world atlas
 d3.json("data/world-110m.json", function(error, data) {
@@ -19,5 +31,6 @@ d3.json("data/world-110m.json", function(error, data) {
     .data(geoJSON)
     .enter()
     .append("path")
+    .attr("class", "map")
     .attr("d", path);
 });
