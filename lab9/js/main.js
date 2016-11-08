10e39313043025a9dbbaacc66077a04e60af1607
Neel Mehta <neelmehta@college.harvard.edu>
2016-11-08 14:45:48 -0500

    add lab9

---
+++ lab9/js/main.js
@@ -0,0 +1,27 @@
+
+var allData = [];
+
+// Variable for the visualization instance
+var stationMap;
+
+// Start application by loading the data
+loadData();
+
+
+function loadData() {
+
+	// Proxy url
+	var proxy = 'http://michaeloppermann.com/proxy.php?format=xml&url=';
+
+  // Hubway XML station feed
+  var url = 'https://www.thehubway.com/data/stations/bikeStations.xml';
+
+  // TO-DO: LOAD DATA
+}
+
+
+function createVis() {
+
+  // TO-DO: INSTANTIATE VISUALIZATION
+
+}
