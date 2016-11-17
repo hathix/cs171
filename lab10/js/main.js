83a34f4cd6b1052285010ead069f0bd8aef83362
Neel Mehta <neelmehta@college.harvard.edu>
2016-11-17 14:25:18 -0500

    add queue

--- lab10/js/main.js
+++ lab10/js/main.js
@@ -1,1 +1,7 @@
-new MatrixVis("matrix-holder", [[1,2],[3,4]]);
+d3.queue()
+  .defer(d3.csv, "data/florentine-family-attributes.csv")
+  .defer(d3.json, "data/matrices.json")
+  .await(function(error, florentineFamilyData, matrixData) {
+
+    new MatrixVis("matrix-holder", matrixData.marriages);
+  });
