f0c5919f0b911e44349510ccc7e477c2baa49a25
Neel Mehta <neelmehta@college.harvard.edu>
2016-11-17 14:05:21 -0500

    add d3 grid

---
+++ lab10/js/matrix-vis.js
@@ -0,0 +1,10 @@
+/**
+ * A matrix visualizer.
+ * @param {Object[][]} _data A 2-dimensional matrix of ints to be visualized.
+ */
+MatrixVis = function(_data) {
+    this.data = _data;
+
+
+    this.initVis();
+}
