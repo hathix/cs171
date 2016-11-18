3b160e8375ee7f6a6784459abe24d6cbb35081bf
Neel Mehta <neelmehta@college.harvard.edu>
2016-11-18 11:42:56 -0500

    updates

--- lab10/js/main.js
+++ lab10/js/main.js
@@ -7,24 +7,6 @@ queue()
     if (error) {
       console.error(error);
     }
-    // console.info(florentineFamilyData);
-    // console.info(matrixData);
-
-    // clean up data - convert each to an object
-    // from 0/1's in marriages/business_ties matrices to
-    // { marriage: 1, businessTie: 0  }
-    // var rows = matrixData.marriages.length;
-    // var cols = matrixData.marriages[0].length;
-    // var combinedData = [];
-    // for (var i = 0; i < rows; i++) {
-    //     combinedData[i] = [];
-    //     for (var j = 0; j < cols; j++) {
-    //         combinedData[i][j] = {
-    //             marriage: matrixData.marriages[i][j],
-    //             businessTie: matrixData.business_ties[i][j],
-    //         };
-    //     }
-    // }
 
     matrixVis = new MatrixVis("matrix-holder", florentineFamilyData,
       matrixData.marriages,
